ac6daff52b3e3724acb5746c4ac4c95f7031fe2c
windclara <windclara@gmail.com>
2020-11-20 15:10:19 +0800

    調整天干、世應使用全卦工廠

--- src/gua.generator.ts
+++ src/gua.generator.ts
@@ -1,4 +1,4 @@
-import { Gua, GuaConfiguration, Elements, Relative, EarthlyBranch, ShihYingPosition, HeavenlyStem } from './gua.interface';
+import { Gua, GuaConfiguration } from './gua.interface';
 import { FullGuaFactory, FullGua } from './full-gua-factory';
 
 /**
@@ -105,7 +105,7 @@ export class GuaGenerator {
         gua += this.drawGua(down, 'down', this.YAO_X_POSITION, this.config.DOWN_FIRST_YAO);
         gua += this.drawGua(up, 'up', this.YAO_X_POSITION, this.config.DOWN_FIRST_YAO - this.config.YAO_GAP * 3);
         gua += this.drawEarthlyBranchesAndRelatives(fullGua);
-        gua += this.drawShihYingAndHeavenlyStem(down, up);
+        gua += this.drawShihYingAndHeavenlyStem(fullGua);
         gua += `\n</g>\n`
         return gua;
     }
@@ -139,31 +139,25 @@ export class GuaGenerator {
      * @param down 下卦
      * @param up 上卦
      */
-    private drawShihYingAndHeavenlyStem(down: Gua, up: Gua): string {
+    private drawShihYingAndHeavenlyStem(fullGua: FullGua): string {
+
         let text = '';
-        const shihYingPosition = this.calculateShinYing(down, up);
         const x = 171;
-        const shihY = this.SHIH_FIRST_YAO - this.config.YAO_GAP * (shihYingPosition.shih - 1);
-        const yingY = this.SHIH_FIRST_YAO - this.config.YAO_GAP * (shihYingPosition.ying - 1);
+        const shihY = this.SHIH_FIRST_YAO - this.config.YAO_GAP * (fullGua.HeavenlyStems.shihPosition - 1);
+        const yingY = this.SHIH_FIRST_YAO - this.config.YAO_GAP * (fullGua.HeavenlyStems.yingPosition - 1);
 
         text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="shih" y="${shihY}" x="${x}" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.SHIH_YING_COLOR}">世</text>\n`;
         text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="ying" y="${yingY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.SHIH_YING_COLOR}">應</text>\n`;
 
         const heavenlyStemShihY = shihY - this.config.YAO_GAP;
         const heavenlyStemYingY = yingY - this.config.YAO_GAP;
-        if (shihYingPosition.shih >= 4) {
-            // 世爻在上
-            text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_1" y="${heavenlyStemShihY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${this.getHeavenlyStem(up, 'UP')}</text>\n`;
-            text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_2" y="${heavenlyStemYingY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${this.getHeavenlyStem(down, 'DOWN')}</text>\n`;
-        } else {
-            // 世爻在下
-            text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_1" y="${heavenlyStemShihY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${this.getHeavenlyStem(down, 'DOWN')}</text>\n`;
-            text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_2" y="${heavenlyStemYingY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${this.getHeavenlyStem(up, 'UP')}</text>\n`;
-        }
+
+        text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_1" y="${heavenlyStemShihY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${fullGua.HeavenlyStems.shih}</text>\n`;
+        text += `<text xml:space="preserve" text-anchor="start" font-family="${this.config.FONT_FAMILY}" font-size="18" id="heavenlyStem_2" y="${heavenlyStemYingY}" x="${x}" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="${this.config.HEAVENLY_STEM_COLOR}">${fullGua.HeavenlyStems.ying}</text>\n`;
+
         return text;
     }
 
-    // ************************ step 1: 繪製全卦子功能 START ************************
     /**
      * step 1.1: 繪製全卦的子功能
      * @param gua 卦
@@ -254,269 +248,4 @@ export class GuaGenerator {
     private drawYangYao(id: string, x: number, y: number) {
         return `<line stroke="${this.config.YAO_COLOR}" id="${id}" x1="${x}" y1="${y}" x2="${x + this.config.YANG_LENGTH}" y2="${y}" stroke-width="${this.config.YAO_BOLD}" fill="none"/>\n`;
     }
-    // ************************ step 1: 繪製全卦子功能 END ************************
-    // ************************ step 2: 裝卦:地支、六親 子功能 START ************************
-
-    /**
-     * 根據宮與地支，取得該爻之六親
-     * @param gung 宮
-     * @param earthlyBranch 地支
-     * @return 該爻六親
-     */
-    private getRelative(gung: Elements, earthlyBranch: string): Relative {
-        const element = this.getElement(earthlyBranch);
-        if (gung === element) {
-            return '兄弟';
-        }
-        let text: Relative = '' as Relative;
-        switch (gung) {
-            case '金':
-                switch (element) {
-                    case '木':
-                        text = '妻財';
-                        break;
-                    case '水':
-                        text = '子孫';
-                        break;
-                    case '火':
-                        text = '官鬼';
-                        break;
-                    case '土':
-                        text = '父母';
-                        break;
-                }
-                break;
-            case '木':
-                switch (element) {
-                    case '金':
-                        text = '官鬼';
-                        break;
-                    case '水':
-                        text = '父母';
-                        break;
-                    case '火':
-                        text = '子孫';
-                        break;
-                    case '土':
-                        text = '妻財';
-                        break;
-                }
-                break;
-            case '水':
-                switch (element) {
-                    case '金':
-                        text = '父母';
-                        break;
-                    case '木':
-                        text = '子孫';
-                        break;
-                    case '火':
-                        text = '妻財';
-                        break;
-                    case '土':
-                        text = '官鬼';
-                        break;
-                }
-                break;
-            case '火':
-                switch (element) {
-                    case '金':
-                        text = '妻財';
-                        break;
-                    case '木':
-                        text = '父母';
-                        break;
-                    case '水':
-                        text = '官鬼';
-                        break;
-                    case '土':
-                        text = '子孫';
-                        break;
-                }
-                break;
-            case '土':
-                switch (element) {
-                    case '金':
-                        text = '子孫';
-                        break;
-                    case '木':
-                        text = '官鬼';
-                        break;
-                    case '水':
-                        text = '妻財';
-                        break;
-                    case '火':
-                        text = '父母';
-                        break;
-                }
-                break;
-        }
-        return text;
-    }
-
-    /**
-     * 傳入天干、地支，回傳五行 (天干還沒做，用到機會少)
-     * @param type 天干/地支
-     * @return 五行
-     */
-    private getElement(type: string): Elements {
-        let text: Elements = '' as Elements;
-        switch (type) {
-            case '子':
-            case '亥':
-                text = '水';
-                break;
-            case '寅':
-            case '卯':
-                text = '木';
-                break;
-            case '巳':
-            case '午':
-                text = '火';
-                break;
-            case '申':
-            case '酉':
-                text = '金';
-                break;
-            case '辰':
-            case '未':
-            case '戌':
-            case '丑':
-                text = '土';
-                break;
-        }
-        return text;
-    }
-    // ************************ step 2: 裝卦:地支、六親 子功能 END ************************
-    // ************************ step 3: 裝卦:天干、世爻 子功能 START ************************
-    /**
-     * 計算幾世卦
-     * @param down 下卦
-     * @param up 上卦
-     * @return 幾世卦
-     */
-    private calculateShinYing(down: Gua, up: Gua): ShihYingPosition {
-
-
-        if (down === up) {
-            return { shih: 6, ying: 3 };
-        }
-
-        if ((up === '天' && down === '風') ||
-            (up === '風' && down === '天') ||
-            (up === '澤' && down === '水') ||
-            (up === '水' && down === '澤') ||
-            (up === '火' && down === '山') ||
-            (up === '山' && down === '火') ||
-            (up === '雷' && down === '地') ||
-            (up === '地' && down === '雷')) {
-            return { shih: 1, ying: 4 };
-        }
-
-        if ((up === '天' && down === '山') ||
-            (up === '山' && down === '天') ||
-            (up === '澤' && down === '地') ||
-            (up === '地' && down === '澤') ||
-            (up === '火' && down === '風') ||
-            (up === '風' && down === '火') ||
-            (up === '雷' && down === '水') ||
-            (up === '水' && down === '雷')) {
-            return { shih: 2, ying: 5 };
-        }
-
-        if ((up === '天' && down === '地') ||
-            (up === '地' && down === '天') ||
-            (up === '澤' && down === '山') ||
-            (up === '山' && down === '澤') ||
-            (up === '火' && down === '水') ||
-            (up === '水' && down === '火') ||
-            (up === '雷' && down === '風') ||
-            (up === '風' && down === '雷') ||
-            (up === '天' && down === '火') ||
-            (up === '火' && down === '天') ||
-            (up === '澤' && down === '雷') ||
-            (up === '雷' && down === '澤') ||
-            (up === '風' && down === '山') ||
-            (up === '山' && down === '風') ||
-            (up === '地' && down === '水') ||
-            (up === '水' && down === '地')) {
-            return { shih: 3, ying: 6 };
-        }
-
-        if ((up === '天' && down === '雷') ||
-            (up === '雷' && down === '天') ||
-            (up === '澤' && down === '火') ||
-            (up === '火' && down === '澤') ||
-            (up === '地' && down === '風') ||
-            (up === '風' && down === '地') ||
-            (up === '山' && down === '水') ||
-            (up === '水' && down === '山') ||
-            (up === '天' && down === '水') ||
-            (up === '水' && down === '天') ||
-            (up === '澤' && down === '風') ||
-            (up === '風' && down === '澤') ||
-            (up === '火' && down === '地') ||
-            (up === '地' && down === '火') ||
-            (up === '雷' && down === '山') ||
-            (up === '山' && down === '雷')) {
-            return { shih: 4, ying: 1 };
-        }
-
-        if ((up === '天' && down === '澤') ||
-            (up === '澤' && down === '天') ||
-            (up === '火' && down === '雷') ||
-            (up === '雷' && down === '火') ||
-            (up === '風' && down === '水') ||
-            (up === '水' && down === '風') ||
-            (up === '山' && down === '地') ||
-            (up === '地' && down === '山')) {
-            return { shih: 5, ying: 2 };
-        }
-
-        return { shih: 0, ying: 0 };
-    }
-
-    /**
-     * 取得天干
-     * @param gua 卦
-     * @param type 上或下卦 (僅坤使用)
-     */
-    private getHeavenlyStem(gua: Gua, type: 'UP' | 'DOWN'): HeavenlyStem {
-        let text = '' as HeavenlyStem;
-        switch (gua) {
-            case '天':
-                text = '甲';
-                break;
-            case '澤':
-                text = '丁';
-                break;
-            case '火':
-                text = '己';
-                break;
-            case '雷':
-                text = '庚';
-                break;
-            case '風':
-                text = '辛';
-                break;
-            case '水':
-                text = '戊';
-                break;
-            case '山':
-                text = '丙';
-                break;
-            case '地':
-                switch (type) {
-                    case 'UP':
-                        text = '癸';
-                        break;
-                    case 'DOWN':
-                        text = '乙';
-                        break;
-                }
-                break;
-        }
-        return text;
-    }
-    // ************************ step 3: 裝卦:天干、世爻 子功能 END ************************
 }
