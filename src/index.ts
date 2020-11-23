7bdb4878d3d05d5d6e74764e9261a936028de96d
windclara <windclara@gmail.com>
2020-11-23 17:31:38 +0800

    調整正式產圖區寫法

--- src/index.ts
+++ src/index.ts
@@ -13,31 +13,28 @@ console.log(res);
 // const chang = fullGuaFactory.create('火', '風');
 // console.log(chang);
 
-
-
-
 // 正式產圖區。要產時放開註解
 // svgGenerator();
 
 function svgGenerator() {
-    const guaArray: Array<{ id: number, gua: Gua }> = [
-        { id: 1, gua: '天' },
-        { id: 2, gua: '澤' },
-        { id: 3, gua: '火' },
-        { id: 4, gua: '雷' },
-        { id: 5, gua: '風' },
-        { id: 6, gua: '水' },
-        { id: 7, gua: '山' },
-        { id: 8, gua: '地' }
-    ];
-
-    console.log(`產生svg中…`);
-    for (const i of guaArray) {
-        for (const j of guaArray) {
-            console.log(`目前產生的卦：${i.gua}${j.gua}`);
-            const svg = guaGen.buildGua(i.gua, j.gua);
-            fs.writeFileSync(`${process.cwd()}/genSVGFile/${i.id}${j.id}.svg`, svg);
-        }
+  const guaArray: Array<{ id: number; gua: Gua }> = [
+    { id: 1, gua: '天' },
+    { id: 2, gua: '澤' },
+    { id: 3, gua: '火' },
+    { id: 4, gua: '雷' },
+    { id: 5, gua: '風' },
+    { id: 6, gua: '水' },
+    { id: 7, gua: '山' },
+    { id: 8, gua: '地' },
+  ];
+  const guaGen = new GuaGenerator();
+  console.log(`產生svg中…`);
+  for (const i of guaArray) {
+    for (const j of guaArray) {
+      console.log(`目前產生的卦：${i.gua}${j.gua}`);
+      const svg = guaGen.buildGua(i.gua, j.gua);
+      fs.writeFileSync(`${process.cwd()}/genSVGFile/${i.id}${j.id}.svg`, svg);
     }
-    console.log(`產生svg完成！`);
+  }
+  console.log(`產生svg完成！`);
 }
