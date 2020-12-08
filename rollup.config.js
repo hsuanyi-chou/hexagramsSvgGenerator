d61b26e6a903875c08aa5a2a2e9603c187ffe3dd
windclara@gmail.com <windclara@gmail.com>
2020-12-08 22:03:27 +0800

    調整rollup寫法

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
 import typescript from 'rollup-plugin-typescript2'
 import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default [
   {
     input: './src/index.ts',
     output: {
-      file: './lib/index.esm.js',
+      file: './lib/index.js',
       format: 'esm',
     },
     plugins: [
-      nodeResolve(),
-      typescript(),
-    ],
-  },
-  {
-    input: './src/index.ts',
-    output: {
-      file: './lib/index.js',
-      format: 'cjs',
-    },
-    plugins: [
-      nodeResolve(),
-      typescript(),
+      typescript({
+        verbosity: 3,
+        tsconfigOverride: {
+          compilerOptions: {
+            module: "ESNext"
+          }
+        },
+        clean: true,
+        check: true,
+        tsconfig: './tsconfig.json',
+        typescript: require('typescript'),
+        useTsconfigDeclarationDir: true
+    }),
+      nodeResolve({dedupe:['lunar-calendar-zh']}),
+      commonjs({transformMixedEsModules: true, include:['.js', '.cjs']}),
     ],
   },
   
